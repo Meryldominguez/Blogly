9358fa815577ff6116110372e590f023d95fc17f
Meryl Dominguez <meryldominguez@gmail.com>
2020-10-27 18:34:09 -0500

    Revert "Revert "Broken""
    This reverts commit 803ab20d344e760d11f56c815c57be121870fae8.

--- seed.py
+++ seed.py
@@ -1,4 +1,4 @@
-from models import db, User, Post
+from models import db, User, Post, Tag, PostTag
 from app import app
 import datetime, time
 
@@ -10,6 +10,7 @@ db.create_all()
 # If table isn't empty, empty it
 User.query.delete()
 Post.query.delete()
+Tag.query.delete()
 # Add Users
 a = User(first_name="Test1", last_name="User1")
 b = User(first_name="Test2", last_name="User2")
@@ -22,11 +23,20 @@ f = User(first_name="Test6", last_name="User6")
 db.session.add_all([a,b,c,d])
 db.session.commit()
 
-pa = Post(post_title="TestPost1", post_content="User1",author=a)
-pb = Post(post_title="TestPost2", post_content="User2",author=b)
-pc = Post(post_title="TestPost3", post_content="User1",author=a)
-pd = Post(post_title="TestPost4", post_content="User4",author=d)
-pe = Post(post_title="TestPost5", post_content="User3",author=c)
+ta = Tag(name="testy1")
+tb = Tag(name="testy2")
+tc = Tag(name="testy3")
+td = Tag(name="testy4")
+te = Tag(name="testy5")
+
+db.session.add_all([ta,tb,tc,td,te])
+db.session.commit()
+
+pa = Post(post_title="TestPost1", post_content="User1",author=a, tags=[ta,tc,te])
+pb = Post(post_title="TestPost2", post_content="User2",author=b, tags=[tb,td,te,ta])
+pc = Post(post_title="TestPost3", post_content="User1",author=a, tags=[tb])
+pd = Post(post_title="TestPost4", post_content="User4",author=d, tags=[tb,td])
+pe = Post(post_title="TestPost5", post_content="User3",author=c, tags=[ta,te])
 
 # Commit--otherwise, this never gets saved!
 
